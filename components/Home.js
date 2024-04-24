79caf115d3882dac30bacff17be9bf1f92b867e7
Louis Chatenay <louis.chatenay11@gmail.com>
2024-04-24 16:51:24 +0200

    commit

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesFromAPI, setMoviesFromAPI] = useState([]);
 
   useEffect(() => {
-    fetch('mymoviz-frontend-pi-tan.vercel.app/movies')
+    fetch('https://mymoviz-backend-three-gamma.vercel.app/movies')
     .then(response => response.json())
     .then(data => {
       setMoviesFromAPI(data.movies)
